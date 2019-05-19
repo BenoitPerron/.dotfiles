908657a59217ab028d4dad507fca5d791446b5c9
Benoit Perron <benoitperron@acm.org>
2019-05-19 00:44:32 -0400

    i3 config

---
+++ .config/i3/nouveau.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-1 --off --output DP-1 --off --output HDMI-1 --mode 1600x900 --pos 0x200 --rotate normal --output DVI-I-2 --mode 1600x1200 --pos 1600x0 --rotate normal
---
+++ .config/i3/screenlayout_new.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-1 --mode 1600x900 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DVI-I-2 --primary --mode 1600x1200 --pos 1600x0 --rotate normal
